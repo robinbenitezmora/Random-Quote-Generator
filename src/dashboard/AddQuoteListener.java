36eb5f1f669f5981d1fc3fabf78df690b92151ce
robinbenitezmora <robinbenitez1990@gmail.com>
2024-01-17 20:20:24 -0500

    Refactor AddQuotesListener.java class

--- src/dashboard/AddQuoteListener.java
+++ src/dashboard/AddQuoteListener.java
@@ -5,50 +5,31 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class AddQuoteListener implements ActionListener {
- private ArrayList<Quote> quotes;
- private JFrame frame;
-
- @Override
+  @Override
   public void actionPerformed(ActionEvent e) {
-  JTextField quote = new JTextField();
-  JTextField authorFirstName = new JTextField();
-  JTextField authorLastName = new JTextField();
-
-  Object[] message = {
-   "Quote:", quote,
-   "Author's First Name:", authorFirstName,
-   "Author's Last Name:", authorLastName
+    new JTextField();
+    new JTextField();
+    new JTextField();
   }
 
- private void addQuote(JTextField quote, JTextField authorFirstName, JTextField authorLastName, Object[] message) {
-  int option = JOptionPane.showConfirmDialog(frame, message, "Add Quote", JOptionPane.OK_CANCEL_OPTION);
-  if (option == JOptionPane.OK_OPTION) {
-   quotes.add(new Quote(quote.getText(), authorFirstName.getText(), authorLastName.getText()));
+  public class Quote {
+    public String quote;
+    public String authorFirstName;
+    public String authorLastName;
+
+    public Quote(String quote, String authorFirstName, String authorLastName) {
+      this.quote = quote;
+      this.authorFirstName = authorFirstName;
+      this.authorLastName = authorLastName;
+    }
   }
- }
 
- // Define a constructor for the Quote class that accepts three string parameters
- public class Quote {
-  private String quote;
-  private String authorFirstName;
-  private String authorLastName;
-
-  public Quote(String quote, String authorFirstName, String authorLastName) {
-   this.quote = quote;
-   this.authorFirstName = authorFirstName;
-   this.authorLastName = authorLastName;
+  public void setQuotes(ArrayList<Quote> quotes) {
   }
- }
 
- public void setQuotes(ArrayList<Quote> quotes) {
-  this.quotes = quotes;
- }
-
- public void setFrame(JFrame frame) {
-  this.frame = frame;
- }
+  public void setFrame(JFrame frame) {
+  }
 }
