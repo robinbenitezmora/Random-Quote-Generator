233d77dc25f28a5318e612894fc5ba10587fcb9b
robinbenitezmora <robinbenitez1990@gmail.com>
2024-01-17 19:54:14 -0500

    Create 'LikeListener.java' class

---
+++ src/dashboard/LikeListener.java
@@ -0,0 +1,17 @@
+package dashboard;
+
+import java.util.ArrayList;
+
+public class LikeListener {
+
+ public void setquoteIndex(int quoteIndex) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setquoteIndex'");
+ }
+
+ public void setQuotes(ArrayList<Quote> quotesList) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setQuotes'");
+ }
+
+}
