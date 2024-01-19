126e16dead7e9f4a5cd295a6d6f41e85d9464dc1
robinbenitezmora <robinbenitez1990@gmail.com>
2024-01-18 19:31:40 -0500

    Create Quote.java class

--- src/dashboard/Quote.java
+++ src/dashboard/Quote.java
@@ -1,5 +1,24 @@
 package dashboard;
 
 public class Quote {
+ private final String quote;
+ private Person author;
 
+ public Quote(String quote) {
+  this.quote = quote;
+ }
+
+ public Quote(String quote, Person author) {
+  this.quote = quote;
+  this.author = author;
+ }
+
+ public void setAuthor(Person author) {
+  this.author = author;
+ }
+
+ @Override
+ public String toString() {
+  return quote + " - " + author;
+ }
 }
