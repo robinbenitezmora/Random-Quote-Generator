724171fa784e78b2ce68dcee6b4a62b3ce8094d2
robinbenitezmora <robinbenitez1990@gmail.com>
2024-01-19 21:20:02 -0500

    Refactor Dashboard

---
+++ src/dashboard/Dashboard.java
@@ -0,0 +1,21 @@
+package dashboard;
+
+import java.util.ArrayList;
+
+public class Dashboard {
+
+ public Dashboard(ArrayList<Quote> quotes) {
+  // TODO Auto-generated constructor stub
+ }
+
+ public void setDefaultCloseOperation(int exitOnClose) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setDefaultCloseOperation'");
+ }
+
+ public void setVisible(boolean b) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+ }
+
+}
